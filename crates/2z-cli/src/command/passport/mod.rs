6c0d495aecbdb19c258cc6c1727833d95e901c2f
Karl <214665535+karl-dz@users.noreply.github.com>
2025-08-22 11:52:31 -0500

    2z-cli: add request Solana validator access

--- crates/2z-cli/src/command/passport/mod.rs
+++ crates/2z-cli/src/command/passport/mod.rs
@@ -1,9 +1,17 @@
 use anyhow::{Result, anyhow};
 use clap::{Args, Subcommand};
-use doublezero_passport::state::ProgramConfig;
-use doublezero_program_tools::zero_copy;
+use doublezero_passport::{
+    ID,
+    instruction::{AccessMode, PassportInstructionData, account::RequestAccessAccounts},
+    state::{AccessRequest, ProgramConfig},
+};
+use doublezero_program_tools::{instruction::try_build_instruction, zero_copy};
+use solana_sdk::{bs58, compute_budget::ComputeBudgetInstruction, pubkey::Pubkey};
 
-use crate::rpc::{Connection, SolanaConnectionOptions};
+use crate::{
+    payer::{SolanaPayerOptions, Wallet},
+    rpc::{Connection, SolanaConnectionOptions},
+};
 
 #[derive(Debug, Args)]
 pub struct PassportCliCommand {
@@ -20,6 +28,19 @@ pub enum PassportSubCommand {
         #[command(flatten)]
         solana_connection_options: SolanaConnectionOptions,
     },
+
+    RequestSolanaValidatorAccess {
+        service_key: Pubkey,
+
+        #[arg(long, value_name = "PUBKEY")]
+        node_id: Pubkey,
+
+        #[arg(long, short = 's', value_name = "BASE58_STRING")]
+        signature: String,
+
+        #[command(flatten)]
+        solana_payer_options: SolanaPayerOptions,
+    },
 }
 
 impl PassportSubCommand {
@@ -29,6 +50,20 @@ impl PassportSubCommand {
                 program_config,
                 solana_connection_options,
             } => execute_fetch(program_config, solana_connection_options).await,
+            PassportSubCommand::RequestSolanaValidatorAccess {
+                service_key,
+                node_id,
+                signature,
+                solana_payer_options,
+            } => {
+                execute_request_solana_validator_access(
+                    service_key,
+                    node_id,
+                    signature,
+                    solana_payer_options,
+                )
+                .await
+            }
         }
     }
 }
@@ -55,6 +90,66 @@ async fn execute_fetch(
 
         // TODO: Pretty print.
         println!("Program config: {program_config:?}");
+    }
+
+    Ok(())
+}
+
+//
+// PassportSubCommand::RequestSolanaValidatorAccess.
+//
+
+async fn execute_request_solana_validator_access(
+    service_key: Pubkey,
+    node_id: Pubkey,
+    signature: String,
+    solana_payer_options: SolanaPayerOptions,
+) -> Result<()> {
+    let wallet = Wallet::try_from(solana_payer_options)?;
+    let wallet_key = wallet.pubkey();
+
+    let decoded_signature = bs58::decode(&signature)
+        .into_vec()
+        .map_err(|_| anyhow!("Invalid base58 string"))?;
+
+    if decoded_signature.len() != 64 {
+        return Err(anyhow!("Invalid signature length"));
+    }
+
+    let mut ed25519_signature = [0u8; 64];
+    ed25519_signature.copy_from_slice(&decoded_signature);
+
+    let request_access_ix = try_build_instruction(
+        &ID,
+        RequestAccessAccounts::new(&wallet_key, &service_key),
+        &PassportInstructionData::RequestAccess(AccessMode::SolanaValidator {
+            validator_id: node_id,
+            service_key,
+            ed25519_signature,
+        }),
+    )?;
+
+    let mut compute_unit_limit = 10_000;
+
+    let (_, bump) = AccessRequest::find_address(&service_key);
+    compute_unit_limit += Wallet::compute_units_for_bump_seed(bump);
+
+    let mut instructions = vec![
+        request_access_ix,
+        ComputeBudgetInstruction::set_compute_unit_limit(compute_unit_limit),
+    ];
+
+    if let Some(ref compute_unit_price_ix) = wallet.compute_unit_price_ix {
+        instructions.push(compute_unit_price_ix.clone());
+    }
+
+    let transaction = wallet.new_transaction(&instructions).await?;
+    let tx_sig = wallet.send_or_simulate_transaction(&transaction).await?;
+
+    if let Some(tx_sig) = tx_sig {
+        println!("Request Solana validator access: {tx_sig}");
+
+        wallet.print_verbose_output(&[tx_sig]).await?;
     }
 
     Ok(())
