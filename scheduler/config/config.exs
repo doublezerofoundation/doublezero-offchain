63fa94c4ad77291da3c64aa8a7d8c8cf99da7a2f
Ben Marx <bgm@malbeclabs.com>
2025-11-14 14:01:31 -0800

    automate initialize distribution (#2141)

--- scheduler/config/config.exs
+++ scheduler/config/config.exs
@@ -3,5 +3,5 @@ import Config
 config :scheduler, Scheduler.Scheduler,
   jobs: [
     {"0 */2 * * *", {Scheduler.Worker.PayDebt, :start_link, []}},
-    {"*/2 * * * *", {Worker.InitializeDistribution, :start_link, []}}
+    {"*/2 * * * *", {Scheduler.Worker.InitializeDistribution, :start_link, []}}
   ]
