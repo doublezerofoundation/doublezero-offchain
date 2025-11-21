d74e8435981a0492a8c273cd01eb2cf0d2828bb7
Ben Marx <bgm@malbeclabs.com>
2025-11-21 10:56:50 -0800

    v0.1.0 (#2193)

--- scheduler/mix.exs
+++ scheduler/mix.exs
@@ -4,7 +4,7 @@ defmodule Scheduler.MixProject do
   def project do
     [
       app: :scheduler,
-      version: "0.1.3",
+      version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
