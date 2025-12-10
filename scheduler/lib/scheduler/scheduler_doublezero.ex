2592ae765a8200577dad8d30f59b82b05ed88e05
Karl <214665535+karl-dz@users.noreply.github.com>
2025-12-10 15:33:34 -0600

    fixup scheduler

--- scheduler/lib/scheduler/scheduler_doublezero.ex
+++ scheduler/lib/scheduler/scheduler_doublezero.ex
@@ -3,7 +3,7 @@ defmodule Scheduler.DoubleZero do
   use Rustler, otp_app: :scheduler, crate: "scheduler_doublezero"
 
   def pay_debt(_dz_epoch, _ledger_rpc, _solana_rpc), do: :erlang.nif_error(:nif_not_loaded)
-  def initialize_distribution(_ledger_rpc, _solana_rpc), do: :erlang.nif_error(:nif_not_loaded)
+  def initialize_distribution(_solana_rpc), do: :erlang.nif_error(:nif_not_loaded)
 
   def calculate_distribution(_dz_epoch, _ledger_rpc, _solana_rpc, _post_to_slack),
     do: :erlang.nif_error(:nif_not_loaded)
